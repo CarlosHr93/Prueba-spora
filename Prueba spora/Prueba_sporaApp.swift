5a70c5ea9059a16dea59985c85aead68ef80cdf2
Carlos Hidalgo <bet0_93@hotmail.com>
2022-07-28 22:03:06 -0500

    Initial Commit

---
+++ Prueba spora/Prueba_sporaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_sporaApp.swift
+//  Prueba spora
+//
+//  Created by Carlos Hidalgo on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_sporaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
